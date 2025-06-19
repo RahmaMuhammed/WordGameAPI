9544ca3cff670401bb10dda443d7caa28b165e95
RahmaMuhammed <rahmamuhammed285@gmail.com>
2025-06-20 00:26:56 +0300

    Add backend API for word selection

---
+++ WordGameAPI/WordGameAPI/Controllers/Word.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WordGameAPI.Controllers
+{
+    public class Word : Controller
+    {
+        [HttpGet("api/words")]
+        public IActionResult GetWord(string category, string difficulty)
+        {
+            var words = HelperClass.LoadWords();
+            var word = words
+           .Where(w => w.category == category && w.difficulty == difficulty)
+           .OrderBy(_ => Guid.NewGuid())
+           .FirstOrDefault();
+
+            if (word == null)
+                return NotFound("No words found.");
+
+            return Ok(new { word = word.word });
+
+        }
+    }
+}
---
+++ WordGameAPI/WordGameAPI/Data/Word.cs
@@ -0,0 +1,6 @@
+﻿public class Word
+{
+    public string word { get; set; }
+    public string category { get; set; }
+    public string difficulty { get; set; }
+}
